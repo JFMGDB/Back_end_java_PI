53a83754473fb4b39a35b0766d168c371af87e22
carloslneeto <161653845+carloslneeto@users.noreply.github.com>
2025-05-24 20:25:14 +0000

    Repositórios

---
+++ inclusiveaid/src/main/java/grupo05/inclusiveaid/repository/AgenteIARepository.java
@@ -0,0 +1,7 @@
+package com.seuusuario.seuprojeto.repository;
+
+import com.seuusuario.seuprojeto.model.AgenteIA;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgenteIARepository extends JpaRepository<AgenteIA, Long> {
+}
---
+++ inclusiveaid/src/main/java/grupo05/inclusiveaid/repository/ContextoRepository.java
@@ -0,0 +1,7 @@
+package com.seuusuario.seuprojeto.repository;
+
+import com.seuusuario.seuprojeto.model.Contexto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContextoRepository extends JpaRepository<Contexto, Long> {
+}
